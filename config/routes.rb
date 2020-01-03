fe8e18701a6d49e9641f912aacb25d3613500bd5
MaxAlmeida <llewxam100@hotmail.com>
2020-01-03 15:59:21 -0300

    Adding routes to event comments and signup user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events do
+    resources :comments
+  end
+
+  post 'signup', to: 'users#create'
 end
