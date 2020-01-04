9c943ebeb67f940edd4708d7c13f2c43bc297eff
MaxAlmeida <llewxam100@hotmail.com>
2020-01-04 18:40:37 -0300

    Fixing report model test

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Report, type: :model do
   end
   
   describe 'validations' do 
+    let!(:event) {create(:event)}
+    let!(:user) {create(:user)}
+    let!(:comment) {create(:comment, user_id: user.id, event_id: event.id)}
+    let!(:report){create(:report, user_id: user.id, comment_id: comment.id)}
+
     it { should validate_uniqueness_of(:user_id).scoped_to(:comment_id) }
   end
 end
