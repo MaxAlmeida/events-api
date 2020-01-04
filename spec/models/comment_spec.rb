ac5cc189238da9c28c51edbdf8f15c41d843a94e
MaxAlmeida <llewxam100@hotmail.com>
2020-01-04 14:28:52 -0300

    create test to reported method from comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Comment, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:text)}
   end
+
+  describe 'reported?' do
+    let!(:event) {create(:event)}
+    let!(:user) {create(:user)}
+    let!(:comment) {create(:comment, user_id: user.id, event_id: event.id)}
+    let!(:comment_without_report) {create(:comment, user_id: user.id, event_id: event.id)}
+    let!(:report){create(:report, user_id: user.id, comment_id: comment.id)}
+
+    context "when comment have not report" do
+      it{ expect(comment_without_report.reported?).to eq(false) }
+    end
+    
+    context "when comment have report" do
+      it{ expect(comment.reported?).to eq(true) }
+    end
+  end
 end
