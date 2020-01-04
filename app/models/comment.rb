c500ba2d02fbe5b7a519fb0f63849324ddf746de
MaxAlmeida <llewxam100@hotmail.com>
2020-01-04 14:28:16 -0300

    create reported check to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :event
   has_many :reports
   validates_presence_of :text
+
+  def reported?
+    self.reports.count > 0 ? true : false  
+  end
+
 end
